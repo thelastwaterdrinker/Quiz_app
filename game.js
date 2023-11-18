b0b402e761d68780e81ba37db512e97928ab8b17
Tugba Dogan <104082640+thelastwaterdrinker@users.noreply.github.com>
2023-11-18 20:10:59 +0100

    Add files via upload

--- game.js
+++ game.js
@@ -2,6 +2,7 @@ const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
 const progressText = document.getElementById("progressText");
 const scoreText = document.getElementById("score");
+const progressBarFull = document.getElementById("progressBarFull");
 let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
@@ -9,54 +10,54 @@ let questionCounter = 0;
 let availableQuesions = [];
 
 let questions = [
-    {
-        question: 'How many rings are on the Olympic flag?',
-        choice1: 'None',
-        choice2: '4',
-        choice3: '5',
-        choice4: '7',
-        answer: 3
-    },
-    {
-        question: 'How did Spider-Man get his power?',
-        choice1: 'Bitten by a radioactive spider',
-        choice2: 'Born with item',
-        choice3: 'Military experiment gone awry',
-        choice4: 'Woke up with them after a strange dream',
-        answer: 1
-    },
-    {
-        question: 'What are the main colors on the flag of Spain?',
-        choice1: 'Black and yellow',
-        choice2: 'Blue and white',
-        choice3: 'Green and white',
-        choice4: 'Red and yellow',
-        answer: 4
-    },
-    {
-        question: 'Who killed Greedo?',
-        choice1: 'Hannibal Lecter',
-        choice2: 'Han Solo',
-        choice3: 'Hermione Granger',
-        choice4: 'Hercules',
-        answer: 2
-    },
-    {
-        question: 'Which of these is virus?',
-        choice1: 'Chicken pox',
-        choice2: 'Leukemia',
-        choice3: 'Scoliosis',
-        choice4: 'Staphylococcus',
-        answer: 1
-    },
-    {
-        question: 'What is the pH of pure water?',
-        choice1: '0',
-        choice2: '1',
-        choice3: '7',
-        choice4: '273',
-        answer: 3
-    },
+  {
+      question: 'How many rings are on the Olympic flag?',
+      choice1: 'None',
+      choice2: '4',
+      choice3: '5',
+      choice4: '7',
+      answer: 3
+  },
+  {
+      question: 'How did Spider-Man get his power?',
+      choice1: 'Bitten by a radioactive spider',
+      choice2: 'Born with item',
+      choice3: 'Military experiment gone awry',
+      choice4: 'Woke up with them after a strange dream',
+      answer: 1
+  },
+  {
+      question: 'What are the main colors on the flag of Spain?',
+      choice1: 'Black and yellow',
+      choice2: 'Blue and white',
+      choice3: 'Green and white',
+      choice4: 'Red and yellow',
+      answer: 4
+  },
+  {
+      question: 'Who killed Greedo?',
+      choice1: 'Hannibal Lecter',
+      choice2: 'Han Solo',
+      choice3: 'Hermione Granger',
+      choice4: 'Hercules',
+      answer: 2
+  },
+  {
+      question: 'Which of these is virus?',
+      choice1: 'Chicken pox',
+      choice2: 'Leukemia',
+      choice3: 'Scoliosis',
+      choice4: 'Staphylococcus',
+      answer: 1
+  },
+  {
+      question: 'What is the pH of pure water?',
+      choice1: '0',
+      choice2: '1',
+      choice3: '7',
+      choice4: '273',
+      answer: 3
+  },
 ];
 
 //CONSTANTS
